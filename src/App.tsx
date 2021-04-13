2770d190166d4075c67b0c2f06c372b371cd3dce
Cri7Ro <kri7ro@mail.ru>
2021-04-13 17:21:17 +0300

    добавлен поиск спустя секунду после ввода последнего символа

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
 function App() {
@@ -8,6 +8,9 @@ function App() {
   const [cover, setCover] = useState<JSX.Element>();
   const [bookTitle, setBookTitle] = useState<string>('');
   const [author, setAuthor] = useState<string>('');
+  const [timerID, setTimerID] = useState<number>(0);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [controller, setController] = useState<AbortController>(new AbortController());
 
   useEffect(() => {
     if (bookList) {
@@ -24,23 +27,42 @@ function App() {
     }]);
   }, [cover]);
 
+  useEffect(() => {
+    if (timerID) clearTimeout(timerID);
+    if (!loader) setTimerID(window.setTimeout(() => bookRequest(), 1000));
+  }, [userInputTitle])
+
+  function bookRequest(): void {
+    setBookList(null);
+    setLoader(true);
+    console.log(userInputTitle);
+    fetch(`http://openlibrary.org/search.json?title=${userInputTitle}`, {
+      signal: controller.signal
+    })
+    .then(response => response.json())
+    .then(data => data.docs.map((e: any, i: number) => {
+        if (e.cover_i) {
+          setBookTitle(e.title);
+          setAuthor(e.author_name);
+          setCover(e.cover_i);
+        };
+        
+      })
+    )
+    .catch(err => console.log('KU'))
+    .finally(() => setLoader(false));
+  }
+
   function handleSubmit(event: React.SyntheticEvent): void {
     event.preventDefault();
-    fetch(`http://openlibrary.org/search.json?title=${userInputTitle}`)
-      .then(response => response.json())
-      .then(data => data.docs.map((e: any, i: number) => {
-          if (e.cover_i) {
-            setBookTitle(e.title);
-            setAuthor(e.author_name);
-            setCover(e.cover_i);
-          };
-        })
-      );
+    if (timerID) clearTimeout(timerID);
+    if (!loader) bookRequest();
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" value={userInputTitle} onChange={e => setUserInputTitle(e.target.value)}/>
+      {loader ? <p>Загрузка...</p> : 
       <ul>
         {
           bookList ? bookList.map((e: any, i: number) =>  
@@ -52,7 +74,7 @@ function App() {
             ) 
           : false
         }
-      </ul>
+      </ul>}
       <input type="submit"/>
     </form>
   );
